47ef082491a49b7dfa48e3526e5ee8eafb4a2eff
GS-dev123 <gsservice907@gmail.com>
2020-05-22 00:42:45 +0200

    PHP

--- processo.php
+++ processo.php
@@ -1,27 +1,44 @@
+
 <?php
+require_once('./src/PHPMailer.php');
+require_once('./src/SMTP.php');
+require_once('./src/Exception.php');
+ 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-if (isset($_POST['email'])){
+$nome = isset($_POST['nome']) ? $_POST['nome'] : 'Nao Informado';
+$email = isset($_POST['email']) ? $_POST['email'] : 'Nao Informado';
+$comentario = isset($_POST['comentario']) ? $_POST['comentario'] : 'Nao Informado';
+ 
+ if($email && $comentario){
+    $mail = new PHPMailer(true);
+    try {
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'germildosilva@gmail.com';
+	$mail->Password = '849300335';
+	$mail->Port = 587;
+ 
+	$mail->setFrom('germildosilva@gmail.com');
+	$mail->addAddress('germildosilva@gmail.com');
+ 
+	$mail->isHTML(true);
+	$mail->Subject = 'Teste de email via gmail Canal TI';
+	$mail->Body = 'Chegou o email teste do <strong>Canal TI</strong>';
+	$mail->AltBody = 'Chegou o email teste do Canal TI';
 
-$nome = addslashes($_POST['nome']);
-$email = addslashes($_POST['email']);
-$comentario = addslashes($_POST['comentario']);
-
-
-$para = "germildosilva@gmail.com";
-$assunto = "WebSite ThulinesArqEng";
-$corpo = "Nome: ".$nome. "\r\n".
-         "Email: ".$email. "\r\n".
-         "comentario: ".$comentario;
-$cabecalho = "From: gsservice907@gmail.com". "\r\n". 
-             "Reply-To ".$email."\r\n".
-             "X=Mailer:PHP/".phpversion();
-
-if(mail($para,$assunto,$corpo,$cabecalho)){
-
-	echo "Email enviado com sucesso!";
+	if($mail->send()) {
+		echo 'Email enviado com sucesso';
+	} else {
+		echo 'Email nao enviado';
+	}
+	} catch (Exception $e) {
+	echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
 }else{
-	echo "Erro no envio do email";
+	echo "Email ou comentario errado";
 }
-}
-
-?>
